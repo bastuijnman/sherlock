81afdab96666523db9d85ff75901c3d708dee6df
Sander Sikkema <sander.sikkema@hva.nl>
2014-12-11 23:21:54 +0100

    Implemented the network logger into the debug client with callbacks for request/responses

--- src/client/client.js
+++ src/client/client.js
@@ -1,9 +1,21 @@
 import Logger from 'logger';
+import NetworkLogger from 'network';
 
 if (typeof io !== 'undefined') {
 
     var socket = new io('http://localhost:7890'),
-        logger = new Logger(socket);
+        logger = new Logger(socket),
+        networkLogger = new NetworkLogger(
+            function(xhr) {
+                // TODO: emit data to socket with type 'request'
+            },
+            function(xhr) {
+                socket.emit('networkconsole', {
+                    'type': 'response',
+                    'data': xhr
+                });
+            }
+        );
 
     /**
      * Dirty hack to send loaded DOM when window is loaded
