c3fa9db4b165b3605cf0f58d11bef8b5c3859308
Sander Sikkema <sander.sikkema@hva.nl>
2014-12-11 23:13:53 +0100

    Added network client component that extends the XmlHttpRequest send function

---
+++ src/client/network.js
@@ -0,0 +1,38 @@
+export default function NetworkLogger(requestCallback, responseCallback) {
+    var COMPLETED_READY_STATE = 4;
+
+    function callResponseCallback(xhr) {
+        if(xhr.readyState === COMPLETED_READY_STATE) {
+            responseCallback(xhr);
+        }
+    }
+
+    function extendOnReadyStateChange(xhr) {
+        (function(onreadystatechange) {
+            xhr.onreadystatechange = function() {
+                if(xhr.readyState === COMPLETED_READY_STATE) {
+                    callResponseCallback(xhr);
+                }
+                onreadystatechange();
+            }
+        }(xhr.onreadystatechange))
+    }
+
+    (function(send) {
+        XMLHttpRequest.prototype.send = function () {
+            // TODO: Need fix for the request when calling the request callback
+            // The reqeust is now not being sent when calling the callback
+            //requestCallback(this);
+            if (this.addEventListener) {
+                var self = this;
+                this.addEventListener("readystatechange", function () {
+                    callResponseCallback(self);
+                }, false);
+            } else {
+                extendOnReadyStateChange(this);
+            }
+            send.apply(this, arguments);
+        }
+
+    }(XMLHttpRequest.prototype.send));
+};
