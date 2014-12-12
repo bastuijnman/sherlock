a4ff1c211b943619dd20ee714dae4b619dd3c69a
Bas Tuijnman <bas.tuijnman@24i.com>
2014-12-12 11:23:36 +0100

    added styling for network

--- src/debugger/components/network/item.js
+++ src/debugger/components/network/item.js
@@ -9,7 +9,16 @@ var Item = React.createClass({
     render: function() {
         var classes = ['item'],
             item = this.props.item,
-            filename = item.response.url.split('/').slice(-1)[0];
+            filename = item.response.url.split('/').slice(-1)[0],
+            filepath = item.response.url.split('/');
+
+        // Remove filename from path
+        filepath.pop();
+        filepath = filepath.join('/');
+        if(filename === '') {
+            filename = filepath;
+            filepath = '/';
+        }
 
         if(!this.state.collapsed) {
             classes.push('active');
@@ -17,7 +26,9 @@ var Item = React.createClass({
 
         return React.createElement('div', {className: classes.join(' '), onClick: this.toggle},
             React.createElement('div', {className: 'info'},
-                React.createElement('span', {className: 'filename'}, filename)
+                React.createElement('span', {className: 'status'}, item.request.status),
+                React.createElement('span', {className: 'filename'}, filename),
+                React.createElement('span', {className: 'filepath'}, filepath)
             )
         );
     },
